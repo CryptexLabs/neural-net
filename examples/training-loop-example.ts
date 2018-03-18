9dfbe87aa3e5903ff9cf4bbf55a949ec2b66eb0c
Josh Woodcock <josh.woodcock@opentimeapp.com>
2018-03-17 22:09:17 -0500

    Add training loop example

---
+++ examples/training-loop-example.ts
@@ -0,0 +1,59 @@
+import {NeuralNetService} from "../src/models/NeuralNetService";
+import {RSIConfigNetwork} from "../src/models/network/config/rsi/RSIConfigNetwork";
+import {Market} from "cryptex-shared-models/src/models/market/Market";
+import {S3CSVInputData} from "../src/models/input/S3CSVInputData";
+import {UnsupervisedNetworkTrainingResult} from "../src/interfaces/unsupervised/UnsupervisedNetworkTrainingResult";
+import {UnsupervisedNetworkTrainingPerformanceResult} from "../src/interfaces/unsupervised/UnsupervisedNetworkTrainingPerformanceResult";
+
+let provider = NeuralNetService.getDefaultProvider();
+
+let market = new Market('GDAX', 'BTC', 'USD');
+
+let network = new RSIConfigNetwork(provider, market);
+
+let data = new S3CSVInputData('a-bucket-with-data', 'rsi/dev/data.csv');
+
+let isTraining = true;
+let sessionCount = 0;
+let targetSessionCount = 100;
+
+let loopInterval = setInterval(()=>{
+
+    if(sessionCount >= targetSessionCount){
+        clearInterval(loopInterval);
+    }
+
+    if(sessionCount < targetSessionCount && !isTraining){
+
+        isTraining = true;
+
+        network
+            .train(data)
+            .then((result: UnsupervisedNetworkTrainingResult) => {
+                // Run a backtest or something
+                let performanceResult : UnsupervisedNetworkTrainingPerformanceResult = {
+
+                    getTrainingResultID(): string {
+                        return result.getResultID();
+                    },
+                    getScore(): number {
+                        return 80;
+                    }
+                };
+
+                return Promise.resolve(performanceResult);
+            })
+            .then((result: UnsupervisedNetworkTrainingPerformanceResult) => {
+                return network.scoreTrainingResult(result.getTrainingResultID(), result.getScore())
+            })
+            .then((success: boolean) => {
+                isTraining = false;
+                sessionCount++;
+            })
+            .catch((reason: Error) => {
+                clearInterval(loopInterval);
+                console.error(reason);
+            });
+    }
+
+},10);
